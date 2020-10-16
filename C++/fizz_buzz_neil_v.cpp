ad93eae8a1d8dd6476c7d392e04eaf7d82beea00
Neil Villamizar <neilvillamizar@gmail.com>
2020-10-16 02:44:18 -0400

    Create fizz_buzz_neil_v.cpp

---
+++ C++/fizz_buzz_neil_v.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+void solve_fizz_buzz(int n){
+    
+    if ( n%15 == 0 ){
+        cout << "Fizz Buzz";
+        return;
+    }
+    
+    if( n%3 == 0){
+        cout << "Fizz";
+    }
+    
+    if( n%5 == 0){
+        cout << "Buzz";
+    }
+    
+    cout << n;
+    
+}
+
+int main(){
+
+    int n;
+    cin >> n;
+    
+    for(int i = 1; i <= n; ++i){
+        
+        solve_fizz_buzz(i);
+        
+        if(i == n) cout << ".\n";
+        else cout << ", ";
+        
+    }
+    
+    return 0;
+    
+}
